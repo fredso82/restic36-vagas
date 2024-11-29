bb407e2ba8f1d42f8ffccc3944ea1320c0d73ace
fredso82 <fredso82@gmail.com>
2024-11-29 14:33:10 -0300

    Integração com o sqlite

--- api/repositories/usuariosRepository.js
+++ api/repositories/usuariosRepository.js
@@ -1,50 +1,42 @@
-const { v4: uuidv4 } = require('uuid');
+const Usuario = require('../models/usuario');
 
-let usuarios = [{
-    id: uuidv4(),
-    nome: "admin",
-    email: "admin@email.com",
-    senha: "admin"
-}];
+async function findAll() {
+  return await Usuario.findAll();
+}
 
-function create({ nome, email, senha }) {    
-    const usuario = {
-        id: uuidv4(),
-        nome,
-        email,
-        senha
-    };
-    usuarios.push(usuario);
+async function findById(id) {
+  return await Usuario.findByPk(id);
+}
+
+async function create({ nome, email, senha }) {
+  return await Usuario.create({ nome, email, senha });
+}
+
+async function remove(id) {
+  const user = await Usuario.findByPk(id);
+  if (user) {
+    await user.destroy();
+    return user;
+  }
+  return null;
+}
+
+async function update(id, { nome, email, senha }) {
+  const usuario = await Usuario.findByPk(id);
+  if (usuario) {
+    usuario.nome = nome;
+    usuario.email = email;
+    usuario.senha = senha;
+    await usuario.save();
     return usuario;
-}
-
-function login({email, senha}) {
-    const index = usuarios.findIndex(u => u.email === email && u.senha === senha);
-    if (index === -1) {
-        return null;
-    }
-
-    const usuario = usuarios[index];
-    
-    return {id: usuario.id, nome: usuario.nome, email: usuario.email};
-}
-
-function findAll() {
-    return usuarios;
-}
-
-function findByEmail(email){
-    const index = usuarios.findIndex(u => u.email.trimEnd() === email.trimEnd());
-    if (index === -1) {
-        return null;
-    }
-
-    return usuarios[index];
+  }
+  return null;
 }
 
 module.exports = {
-    create,
-    login,
-    findAll,
-    findByEmail
-}
+  findAll,
+  findById,
+  create,
+  update,
+  remove,
+};
--- api/server.js
+++ api/server.js
@@ -1,63 +1,24 @@
 const express = require('express');
-const { create, update, remove, findAll } = require('./repositories/vagasRepository');
-const usuarioRepository = require('./repositories/usuariosRepository');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/database');
+const usuariosRoutes = require('./routes/usuarios');
+const vagasRoutes = require('./routes/vagas');
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
 
-
-app.use(express.json());
-
-app.post('/vagas', (req, res) => {
-    const { descricao, titulo, dataCadastro, telefone, empresa } = req.body;
-    const vaga = create({ descricao, titulo, dataCadastro, telefone, empresa });
-    res.status(201).json(vaga);
+// Sincronizar o banco de dados
+sequelize.sync().then(() => {
+  console.log('Database synchronized');
+}).catch(err => {
+  console.error('Unable to synchronize the database:', err);
 });
 
-app.get('/vagas', (req, res) => {
-    const vagas = findAll();
-    res.json(vagas);
-});
+// Usar as rotas importadas
+app.use('/usuarios', usuariosRoutes);
+app.use('/vagas', vagasRoutes);
 
-app.put('/vagas/:id', (req, res) => {
-    const { id } = req.params;
-    const { descricao, titulo, dataCadastro, telefone, empresa } = req.body;
-    const vaga = update(id, { descricao, titulo, dataCadastro, telefone, empresa });
-    res.json(vaga);
-});
-
-app.delete('/vagas/:id', (req, res) => {
-    const { id } = req.params;
-    remove(id);
-    res.status(204).send();
-});
-
-app.get('/usuarios', (req, res) => {
-    const usuario = usuarioRepository.findAll();
-    res.json(usuario);
-});
-
-app.post('/registro', (req, res) => {
-    const body = req.body;
-    const usuarioExistente = usuarioRepository.findByEmail(body.email);
-    if (usuarioExistente) {
-        return res.status(400).send("Já existe um usuário com este e-mail");
-    }
-    const usuarios = usuarioRepository.findAll
-    const usuario = usuarioRepository.create(body);
-    res.status(201).json(usuario);
-});
-
-app.post('/login', (req, res) => {
-    const body = req.body;
-    const usuario = usuarioRepository.login(body);
-    if (!usuario) {
-        return res.status(401).send("Login ou senha inválidos");
-    }
-    return res.json(usuario);
-});
-
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
